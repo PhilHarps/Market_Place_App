6a6ccb9a0cc3d1a95e19d5a7ec94e3f963945cdf
PhilHarps <thephilipharper@gmail.com>
2019-11-05 11:54:00 +1000

    cloudinary

--- src/app/models/item.rb
+++ src/app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_many_attached :images
+  has_one_attached :image
 
   validates :description, presence: true
   validates :price, presence: true, numericality: true, numericality: {greater_than: 0}
